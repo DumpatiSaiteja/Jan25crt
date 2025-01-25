daa2772be7589526914a053ba7db48e02a2b8601
DumpatiSaiteja <saitejadumpati3@gmail.com>
2025-01-25 15:08:49 +0530

    Update ArrayDemo3.java

--- ArrayDemo3.java
+++ ArrayDemo3.java
@@ -2,9 +2,13 @@
 public class ArrayDemo3 {
 
 	public static void main(String[] args) {
-		int[] a= {100,200,300,400,500,600,100,200,300,400,500,600};
-		System.out.println("" );
+		int[] num = {100,200,300,400,500,600,100,200,300,400,500,600};
+		//int a[] = new int [5];
+		for(int i=0;i<=num.length;i++) {
+			System.out.print(num[i]+" ");
+		}
 
 	}
 
 }
+
